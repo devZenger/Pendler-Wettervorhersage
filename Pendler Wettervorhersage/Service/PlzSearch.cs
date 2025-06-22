1d0a4b5b62b1e4cd6250a65c36516930d64e2bcf
workinghorse\coderider <alwayscarryatowel@mail.de>
2025-06-22 11:52:23 +0200

    Refactor code for new csv

--- Pendler Wettervorhersage/Service/PlzSearch.cs
+++ Pendler Wettervorhersage/Service/PlzSearch.cs
@@ -1,4 +1,6 @@
 ﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 using System.Globalization;
 using System.IO;
 
@@ -8,15 +10,20 @@ namespace Pendler_Wettervorhersage
     {
         public List<PlzCsv> SearchPlz(int plz)
         {
-            using (var reader = new StreamReader("PLZ.csv"))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            System.Diagnostics.Debug.WriteLine("test");
+            using (var reader = new StreamReader("Resourcen/plz_geo.csv"))
+            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.GetCultureInfo("de-DE"))
+            {
+                Delimiter = ";",
+                HasHeaderRecord = false
+            }))
             {
                 var records = csv.GetRecords<PlzCsv>().ToList();
-
+                System.Diagnostics.Debug.WriteLine($"records {records}");
                 var result = from record in records
                              where record.Plz == plz
                              select record;
-
+                System.Diagnostics.Debug.WriteLine($"result: {result}");
                 return result.ToList();
             }
         }
@@ -24,9 +31,13 @@ namespace Pendler_Wettervorhersage
 
     internal class PlzCsv
     {
+        [Index(0)]
         public int Plz { get; set; }
-        public string City { get; set; } = string.Empty;
-        public string Longitude { get; set; } = string.Empty;
+        //public string City { get; set; } = string.Empty;
+        [Index(1)]
         public string Latidude { get; set; } = string.Empty;
+        [Index(2)]
+        public string Longitude { get; set; } = string.Empty;
+
     }
 }
