d733ec2a7148bf4a973c99a9299fbaf5804f2279
workinghorse\coderider <alwayscarryatowel@mail.de>
2025-06-22 14:59:19 +0200

    Create ApiKey class

---
+++ Pendler Wettervorhersage/Model/ApiKey.cs
@@ -0,0 +1,19 @@
+﻿namespace Pendler_Wettervorhersage
+{
+    internal class ApiKey : NotifyPropertyChangedBase
+    {
+        private string _storedApiKey = "ApiKey eingeben";
+        public string StoredApiKey
+        {
+            get => _storedApiKey;
+            set
+            {
+                if (_storedApiKey != value)
+                {
+                    _storedApiKey = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
