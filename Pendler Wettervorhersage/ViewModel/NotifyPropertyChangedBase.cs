55ba757fe96d5c0ca727d201723a4345fd49249d
workinghorse\coderider <alwayscarryatowel@mail.de>
2025-06-22 14:36:51 +0200

    Clean code

--- Pendler Wettervorhersage/ViewModel/NotifyPropertyChangedBase.cs
+++ Pendler Wettervorhersage/ViewModel/NotifyPropertyChangedBase.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pendler_Wettervorhersage
 {
     internal class NotifyPropertyChangedBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
-
         public virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
